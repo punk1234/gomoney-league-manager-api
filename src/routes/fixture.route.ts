dded44aa57d295d88677d1982339b6dd1ba911bb
AKEJU FATAI <akejufatai@yahoo.com>
2023-01-08 00:20:24 +0100

    [feat] update fixture

--- src/routes/fixture.route.ts
+++ src/routes/fixture.route.ts
@@ -9,6 +9,7 @@ const controller = Container.get(FixtureController);
 router.post("/", requireAuth(true), controller.createFixture);
 
 router.get("/:fixtureId", requireAuth(true), controller.getFixture);
+router.patch("/:fixtureId", requireAuth(true), controller.updateFixture);
 router.delete("/:fixtureId", requireAuth(true), controller.removeFixture);
 
 export default router;
--- src/services/fixture.service.ts
+++ src/services/fixture.service.ts
@@ -1,9 +1,10 @@
+import C from "../constants";
 import { Inject, Service } from "typedi";
-import { CreateFixtureDto } from "../models";
+import { CreateFixtureDto, UpdateFixtureDto } from "../models";
 import { TeamService } from "./team.service";
 import { IFixture } from "../database/types/fixture.type";
 import FixtureModel from "../database/models/fixture.model";
-import { ConflictError, NotFoundError, UnprocessableError } from "../exceptions";
+import { ConflictError, NotFoundError, ServerError, UnprocessableError } from "../exceptions";
 
 @Service()
 export class FixtureService {
@@ -32,6 +33,55 @@ export class FixtureService {
     const FIXTURE = new FixtureModel({ ...data, createdBy: actionBy });
 
     return FIXTURE.save();
+  }
+
+  /**
+   * @method updateFixture
+   * @async
+   * @param {UpdateFixtureDto} data
+   * @param {string} actionBy
+   * @returns {Promise<IFixture>}
+   */
+  async updateFixture(fixtureId: string, data: UpdateFixtureDto, actionBy: string): Promise<IFixture> {
+    if (data.commencesAt && new Date(data.commencesAt).getTime() < Date.now()) {
+      throw new UnprocessableError("Fixture start-date must be in the future");
+    }
+    
+    const FIXTURE = await this.checkThatFixtureExist(fixtureId);
+
+    let { homeTeamId, awayTeamId } = FIXTURE;
+    const FILTERS: Array<string> = [];
+
+    if(data.homeTeamId) {
+      FILTERS.push(data.homeTeamId) && (homeTeamId = data.homeTeamId);
+    }
+
+    if(data.awayTeamId) {
+      FILTERS.push(data.awayTeamId) && (awayTeamId = data.awayTeamId);
+    }
+
+    if(homeTeamId == awayTeamId) {
+      throw new ConflictError("Home & away teams cannot be the same!");
+    }
+
+    if(FILTERS.length) {
+      // NOTE: IF WE HAVE 1 ITEM, AND IT DOES NOT EXIST, ERROR MSG MIGHT BE INVALID SAYING `ONE OR MORE...`
+      await this.teamService.checkThatTeamsExist(FILTERS);
+      await this.checkThatFixtureDoesNotExist(homeTeamId, awayTeamId);
+    }
+
+    // TODO: HANDLE MATCH-RESULT UPDATE
+
+    const UPDATED_FIXTURE = await FixtureModel.findOneAndUpdate(
+        { _id: fixtureId }, { ...data, updatedBy: actionBy },
+        { new: true }
+    );
+
+    if(UPDATED_FIXTURE) {
+      return UPDATED_FIXTURE;
+    }
+
+    throw new ServerError(C.ResponseMessage.ERR_SERVER);
   }
 
   /**
