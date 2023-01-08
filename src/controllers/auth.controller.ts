978ef9083e70373c5675ed18ef9b071dabc44d64
AKEJU FATAI <akejufatai@yahoo.com>
2023-01-08 14:34:02 +0100

    [feat] auth logout (invalidate user-session)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,12 +4,16 @@ import { Request, Response } from "express";
 import { ResponseHandler } from "../helpers";
 import { LoginDto, RegisterUserDto } from "../models";
 import { AuthService } from "../services/auth.service";
+import { SessionService } from "../services/session.service";
 
 @Service()
 @Controller()
 export class AuthController {
   // eslint-disable-next-line no-useless-constructor
-  constructor(@Inject() private authService: AuthService) {}
+  constructor(
+    @Inject() private authService: AuthService,
+    @Inject() private sessionService: SessionService
+  ) {}
 
   /**
    * @method register
@@ -33,5 +37,17 @@ export class AuthController {
     const loginResponse = await this.authService.login(req.body as LoginDto);
 
     ResponseHandler.ok(res, loginResponse);
+  }
+
+  /**
+   * @method logout
+   * @async
+   * @param {Request} req
+   * @param {Response} res
+   */
+  async logout(req: Request, res: Response) {
+    await this.sessionService.invalidateSession(req.auth?.userId as string);
+
+    ResponseHandler.ok(res, { success: true });
   }
 }
