ffafb9687ee19e5c00cd2e024bed6bb4f1e22f8c
AKEJU FATAI <akejufatai@yahoo.com>
2023-01-10 12:24:32 +0100

    [chore] setup tests

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+// Bootstrap Test credentials
+process.env.JWT_TOKEN_SECRET = "jwt_token_secret";
+
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node"
+};
