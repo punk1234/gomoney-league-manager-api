78c3b696eb5ae08cfe2666b20c02d779d50dd174
AKEJU FATAI <akejufatai@yahoo.com>
2023-01-11 21:09:41 +0100

    fix lint

--- src/database/seeders/user-team-fixture.seeder.ts
+++ src/database/seeders/user-team-fixture.seeder.ts
@@ -13,7 +13,7 @@ import { CreateFixtureDto, CreateTeamDto, RegisterUserDto } from "../../models";
 const anyUserExist = async (): Promise<boolean> => {
   const USER = await UserModel.findOne();
   return !!USER;
-}
+};
 
 /******************************************************
  ************** FIXTURES DATA GENERATOR ***************
@@ -21,20 +21,23 @@ const anyUserExist = async (): Promise<boolean> => {
 
 /**
  * @function getFixturesData
- * @param {Array<ITeam>} teams 
- * @returns 
+ * @param {Array<ITeam>} teams
+ * @returns
  */
 const getFixturesData = (teams: Array<ITeam>): Array<CreateFixtureDto> => {
-  const MAX_HOME_MATCH_PER_TEAM: number = 3;
+  const MAX_HOME_MATCH_PER_TEAM = 3;
   const FIXTURES: Array<CreateFixtureDto> = [];
 
-  for(let teamIndex = 0; teamIndex < (teams.length - MAX_HOME_MATCH_PER_TEAM); teamIndex++) {
-
-    for(let awayTeamIndex = (teamIndex+1); awayTeamIndex <= (teamIndex+MAX_HOME_MATCH_PER_TEAM); awayTeamIndex++) {
+  for (let teamIndex = 0; teamIndex < teams.length - MAX_HOME_MATCH_PER_TEAM; teamIndex++) {
+    for (
+      let awayTeamIndex = teamIndex + 1;
+      awayTeamIndex <= teamIndex + MAX_HOME_MATCH_PER_TEAM;
+      awayTeamIndex++
+    ) {
       FIXTURES.push({
         homeTeamId: teams[teamIndex]._id.toString(),
         awayTeamId: teams[awayTeamIndex]._id.toString(),
-        commencesAt: new Date(Date.now() + 3_600_000) // next 1hr
+        commencesAt: new Date(Date.now() + 3_600_000), // next 1hr
       });
     }
   }
@@ -44,13 +47,13 @@ const getFixturesData = (teams: Array<ITeam>): Array<CreateFixtureDto> => {
 
 /**
  * @function seedUsers
- * @param {Array<RegisterUserDto>} data 
+ * @param {Array<RegisterUserDto>} data
  */
 const seedUsers = async (data: Array<RegisterUserDto>) => {
   await UserModel.bulkSave(data.map((item: RegisterUserDto) => new UserModel(item)));
 
   Logger.info("Users have been seeded successfully!!");
-}
+};
 
 /******************************************************
  ******************** TEAMS SEEDER ********************
@@ -59,15 +62,15 @@ const seedUsers = async (data: Array<RegisterUserDto>) => {
 /**
  * @method seedTeams
  * @async
- * @param {Array<CreateTeamDto>} data 
+ * @param {Array<CreateTeamDto>} data
  * @returns {Promise<Array<ITeam>>}
  */
 const seedTeams = async (data: Array<CreateTeamDto>): Promise<Array<ITeam>> => {
   await TeamModel.bulkSave(data.map((item: CreateTeamDto) => new TeamModel(item)));
   Logger.info("Teams have been seeded successfully!!");
-  
+
   return TeamModel.find().limit(data.length);
-}
+};
 
 /******************************************************
  ****************** FIXTURES SEEDER *******************
@@ -76,13 +79,13 @@ const seedTeams = async (data: Array<CreateTeamDto>): Promise<Array<ITeam>> => {
 /**
  * @function seedFixtures
  * @async
- * @param {Array<CreateFixtureDto>} data 
+ * @param {Array<CreateFixtureDto>} data
  */
 const seedFixtures = async (data: Array<CreateFixtureDto>) => {
   await TeamModel.bulkSave(data.map((item: CreateFixtureDto) => new FixtureModel(item)));
-  
+
   Logger.info("Fixtures have been seeded successfully!!");
-}
+};
 
 /******************************************************
  ******************** MAIN SEEDER *********************
@@ -91,22 +94,22 @@ const seedFixtures = async (data: Array<CreateFixtureDto>) => {
 // NOTE: CAN USE DB TRANSACTIONS HERE TO MAKE SEEDING ATOMIC
 export default async function main(): Promise<void> {
   try {
-    if(await anyUserExist()) {
-        Logger.info("SKIPPING >> SEEDING FOR USERS, TEAMS & FIXTURES")
-        return;
-      }
-    
-      Logger.info("READY TO SEED DATABASE WITH DATA!");
-      await seedUsers(USERS_DATA);
-    
-      const TEAMS: Array<ITeam> = await seedTeams(TEAMS_DATA);
+    if (await anyUserExist()) {
+      Logger.info("SKIPPING >> SEEDING FOR USERS, TEAMS & FIXTURES");
+      return;
+    }
 
-      const FIXTURES_DATA = getFixturesData(TEAMS);
-      await seedFixtures(FIXTURES_DATA);
-    
-      Logger.info("SEEDING COMPLETED SUCCESSFULLY!");
-  } catch(err: any) {
+    Logger.info("READY TO SEED DATABASE WITH DATA!");
+    await seedUsers(USERS_DATA);
 
+    const TEAMS: Array<ITeam> = await seedTeams(TEAMS_DATA);
+    const FIXTURES_DATA = getFixturesData(TEAMS);
+
+    await seedFixtures(FIXTURES_DATA);
+    
+    Logger.info("SEEDING COMPLETED SUCCESSFULLY!");
+  } catch (err: any) {
+      
     Logger.error(`[SEEDING FAILED]: ${err.message}`);
     process.exit(1);
   }
